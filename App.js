6bc999c7c5ab7ee944b305e2554978c64e3e942b
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2023-10-12 17:47:07 -0300

    metade da calculadora pronta

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useState } from "react";
+import { Entypo } from '@expo/vector-icons';
 
 export default function App() {
 
   const[darkMode, setDarkMode] = useState(false)
+  const buttons = ['AC', 'DEL', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 3, 2, 1, '+', '.', 0, '+/-', '=']
 
   const styles = StyleSheet.create(
     {
@@ -19,20 +21,65 @@ export default function App() {
       results: {
         backgroundColor: darkMode ? '#282f3b' : '#f5f5f5',
         width: '100%',
-        minHeight: 300
-      }
+        minHeight: 300,
+        alignItems: 'flex-end',
+        justifyContent:'flex-end'
+      },
+
+      resultsText: {
+        margin: 10,
+        fontSize: 25
+      },
+
+      themeButtom: {
+        alignSelf: 'flex-start',
+        bottom: 120,
+        margin: 15,
+        backgroundColor: darkMode ? '#7b8084' : '#e5e5e5',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+      },
+
+      buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+      },
+
+      button: {
+        borderColor: darkMode ? '#3f445b' : '#e5e5e5',
+        borderWidth: 0.7,
+        alignItems: "center",
+        justifyContent: "center",
+        minWidth: 90,
+        minHeight: 90,
+        flex: 2,
+      },
     }
   )
 
   return(
     <View>
       <View style={styles.results}>
-        <TouchableOpacity style={styles.themeButtom}></TouchableOpacity>
+        <TouchableOpacity style={styles.themeButtom}>
+          <Entypo name={darkMode ? 'light-up' : 'moon'} size={24} color={darkMode ? 'white' : 'black'} onPress={() => darkMode ? setDarkMode(false) : setDarkMode(true)} />
+        </TouchableOpacity>
         <Text style={styles.resultsText}>2 + 2 = 5</Text>
       </View>
 
-      <View style={styles.buttom}>
+      <View style={styles.buttons}>
+        {buttons.map((button) => 
+          button === '=' 
+          ? <TouchableOpacity key={button} style={[styles.button, {backgroundColor: '#FFAB5C'}]}>
+            <Text style={[styles.textButton]}>{button}</Text>
+          </TouchableOpacity>
 
+          : <TouchableOpacity key={button} style={[styles.button, {backgroundColor: typeof(button) === 'number' ? darkMode === true ? '#303946' : '#fff' : darkMode == true ? '#414853' : '#ededed'}]}>
+            <Text>{button}</Text>
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   )
