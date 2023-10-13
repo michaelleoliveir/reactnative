90ab46d452cf4862c8f3d7676c5fa5c4cd2364c2
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2023-10-13 10:27:30 -0300

    Revert "tirei os estilos do código principal"
    This reverts commit 9cfbf096b7c588f6db9a3b1e952a439222b0231f.

--- App.js
+++ App.js
@@ -1,15 +1,64 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { useState } from "react";
 import { Entypo } from '@expo/vector-icons';
-import { styles } from './styles';
 
 export default function App() {
 
   const[darkMode, setDarkMode] = useState(false)
-  
   const buttons = ['AC', 'DEL', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 3, 2, 1, '+', '.', 0, '+/-', '=']
+
+  const styles = StyleSheet.create(
+    {
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+
+      results: {
+        backgroundColor: darkMode ? '#282f3b' : '#f5f5f5',
+        width: '100%',
+        minHeight: 300,
+        alignItems: 'flex-end',
+        justifyContent:'flex-end'
+      },
+
+      resultsText: {
+        margin: 10,
+        fontSize: 25
+      },
+
+      themeButtom: {
+        alignSelf: 'flex-start',
+        bottom: 120,
+        margin: 15,
+        backgroundColor: darkMode ? '#7b8084' : '#e5e5e5',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+      },
+
+      buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+      },
+
+      button: {
+        borderColor: darkMode ? '#3f445b' : '#e5e5e5',
+        borderWidth: 0.7,
+        alignItems: "center",
+        justifyContent: "center",
+        minWidth: 90,
+        minHeight: 90,
+        flex: 2,
+      },
+    }
+  )
 
   return(
     <View>
